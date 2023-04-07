744afe3038186b213fadf1dbc81a066cbbb5cb07
Nguyen Anh Tuan <nguyenanhtuan1232@gmail.com>
2023-04-07 20:28:10 +0700

    Raw sign in form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './shared/styles/global.css'
+import Form from './shared/components/Form';
 
 function App() {
-  // const [hasAccount, setHasAccount] = useState(false);
-
-  // const handleYesClick = () => {
-  //   setHasAccount(true);
-  // }
-
-  // const handleNoClick = () => {
-  //   setHasAccount(false);
-  // }
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className='Title'>Welcome to this shit</p>
-        {/* <p className='subTitle'>Do you have an account?</p> */}
+      <img src={logo} className="App-logo" alt="logo" />
+      <header className="formParent">
+        <div className="titleWrapper">
+          <p className='title'>sign in</p>
+          <p className='subTitle'>use your email vs password to sign in</p>
+        </div>
+        <Form onSubmit={()=>{}}/>
       </header>
     </div>
   );
