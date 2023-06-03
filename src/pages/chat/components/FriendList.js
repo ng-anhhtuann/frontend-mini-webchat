8a606bfc4e4ed78c55a73ad534f75ad5057f3f0f
Dang Cong Nhat <dcn22402@gmail.com>
2023-06-03 22:55:43 +0700

    Creating modal for find friends and create new chat

--- src/pages/chat/components/FriendList.js
+++ src/pages/chat/components/FriendList.js
@@ -1,10 +1,29 @@
+import { useState, Fragment } from 'react';
+import Modal from '../../../shared/components/Modal';
 const FriendList = () => {
+    const [showFriendsModal, setShowFriendsModal] = useState(false);
+
     return (
         <div id="friends" className="card-shadow">
             <div id="title">
                 <h3 className="m-0">Contacts</h3>
-                <button>ADD</button>
+                <button onClick={() => setShowFriendsModal(true)}>ADD</button>
             </div>
+            {showFriendsModal && (
+                <Modal click={() => setShowFriendsModal(false)}>
+                    <Fragment key="header">
+                        <h3 className="m-0">Create new chat</h3>
+                    </Fragment>
+
+                    <Fragment key="body">
+                        <p>Find friends by typing their name below</p>
+                        <input type="text" placeholder="Search..." />
+                        <div id="suggestions">
+                            <h1>Friend 1</h1>
+                        </div>
+                    </Fragment>
+                </Modal>
+            )}
         </div>
     );
 };
