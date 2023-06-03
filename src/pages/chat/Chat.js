df9660d928af221820f5a99ce3cd89292fefeb02
Dang Cong Nhat <dcn22402@gmail.com>
2023-06-03 15:20:13 +0700

    Creating Navbar for Chat components

--- src/pages/chat/Chat.js
+++ src/pages/chat/Chat.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import '../../App.scss';
-
+import Navbar from './components/Navbar';
 const Chat = () => {
-    return <div id="chat-container">Chat</div>;
+    return (
+        <div id="chat-container">
+            <Navbar />
+            Chat
+        </div>
+    );
 };
 
 export default Chat;
