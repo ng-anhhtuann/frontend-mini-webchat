21a7b4014f5eba1773de926a0d3418e117401441
Dang Cong Nhat <dcn22402@gmail.com>
2023-06-03 19:32:14 +0700

    Creating FriendList and Messenger components, CSS for notification badge

---
+++ src/pages/chat/components/FriendList.js
@@ -0,0 +1,11 @@
+const FriendList = () => {
+    return (
+        <div id="friends" className="card-shadow">
+            <div id="title">
+                <h3 className="m-0">Contacts</h3>
+                <button>ADD</button>
+            </div>
+        </div>
+    );
+};
+export default FriendList;
---
+++ src/pages/chat/components/Messenger.js
@@ -0,0 +1,8 @@
+const Messenger = () => {
+    return (
+        <div id="messenger" className="card-shadow">
+            <h2>Messenger </h2>
+        </div>
+    );
+};
+export default Messenger;
