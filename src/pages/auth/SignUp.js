062e5789a01e8df5a9b51fd871efe8d14549a29f
ng-anhhtuann <nguyenanhtuan1232@gmail.com>
2023-06-25 15:39:13 +0700

    Navigate routing fix

--- src/pages/auth/SignUp.js
+++ src/pages/auth/SignUp.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import '../../App.scss';
 import Logo from '../../shared/components/Logo';
 import SignUpForm from '../../shared/components/SignUpForm';
+import { useNavigate } from 'react-router-dom';
 
 export default function SignUp() {
+
+    const navigate = useNavigate();
+    useEffect(() => {
+        const userIdSession = sessionStorage.getItem('user');
+        const tokenSession = sessionStorage.getItem('token');
+        if ( userIdSession !== null && tokenSession !== null ) {
+            navigate('chat');
+        }
+    }, [navigate])
+
     return (
         <div id="auth-container">
             <div className="logo-container">
