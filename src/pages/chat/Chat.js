16126b007da261372a3d764864c1dac49b6cde0e
ng-anhhtuann <nguyenanhtuan1232@gmail.com>
2023-06-27 22:20:10 +0700

    Wrap user obj into attribute

--- src/pages/chat/Chat.js
+++ src/pages/chat/Chat.js
@@ -1,24 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../App.scss';
 import Navbar from '../../shared/components/Chat/Navbar';
 import Messenger from '../../shared/components/Chat/Messenger';
 import FriendList from '../../shared/components/Chat/FriendList';
 import { useNavigate } from 'react-router-dom';
+import UserService from '../../shared/service/userService';
 
 const Chat = () => {
+    const [user, setUser] = useState({});
     const navigate = useNavigate();
 
     useEffect(() => {
-        const userIdSession = sessionStorage.getItem('user');
-        const tokenSession = sessionStorage.getItem('token');
+        const userIdSession = sessionStorage.user;
+        const tokenSession = sessionStorage.token;
         if (userIdSession === null || tokenSession === null) {
             navigate('/');
+        } else {
+            UserService.userById(userIdSession).then((res) => {
+                setUser(res.data)
+            });
         }
     }, [navigate]);
 
     return (
         <div id="chat-container">
-            <Navbar />
+            <Navbar userData={user}/>
             <div id="chat-wrap">
                 <FriendList />
                 <Messenger />
