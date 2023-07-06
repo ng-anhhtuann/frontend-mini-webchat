5b0086e1f4a9190d2ffdab3ecd477f291c854e7c
ng-anhhtuann <nguyenanhtuan1232@gmail.com>
2023-07-06 14:54:13 +0700

    Make typical button type for user search exp

--- src/shared/components/chat/FriendList.js
+++ src/shared/components/chat/FriendList.js
@@ -1,4 +1,4 @@
-import { useState, Fragment, useMemo } from 'react';
+import { useState, Fragment, useMemo, useReducer } from 'react';
 import Modal from '../Modal';
 import Button from '../Button';
 import Friend from './Friend';
@@ -17,14 +17,26 @@ const FriendList = ({friendList}) => {
 
     const searchFriends = (e) => {
         ChatService.searchFriends(id, e.target.value).then((res) =>
-            setSuggestions(res.data),
+            setSuggestions(res.data)
         );
     };
 
     const sendFriendRequest = (idAdd) => {
         UserService.addFriend(id, idAdd).then((res) => {
             setIsNotify(true);
-            setTextNotify("Add successfully");
+            setTextNotify("Friend request was sent successfully");
+            setTypeNotify("success")
+        }).catch((err) => {
+            setIsNotify(true);
+            setTextNotify(err);
+            setTypeNotify("error")
+        })
+    }
+
+    const acpFriendRequest = (idAcp) => {
+        UserService.acpFriend(id, idAcp).then((res) => {
+            setIsNotify(true);
+            setTextNotify("You can now text each other");
             setTypeNotify("success")
         }).catch((err) => {
             setIsNotify(true);
@@ -65,7 +77,6 @@ const FriendList = ({friendList}) => {
                     <Fragment key="header">
                         <h3 className="m-0">Create new chat</h3>
                     </Fragment>
-
                     <Fragment key="body">
                         <p>Find friends by typing their name below</p>
                         <input
@@ -80,9 +91,27 @@ const FriendList = ({friendList}) => {
                                         <p className="m-0">
                                             {user.nameDisplay}
                                         </p>
-                                        <button className="btn-success" onClick={() => sendFriendRequest(user.id)}>
+                                        {user.status === 0 
+                                        ? (<button className="btn-success" onClick={() => {
+                                            sendFriendRequest(user.id);
+                                            user.status = 2;
+                                        }}>
                                             ADD
-                                        </button>
+                                        </button>) 
+                                        : user.status === 1 
+                                        ? ( <button className="btn-friend">
+                                            FRIEND
+                                        </button>)
+                                        : user.status === 2 
+                                        ? ( <button className="btn-wait">
+                                            SENT
+                                        </button>)
+                                        : ( <button className="btn-acp" onClick={() => {
+                                            acpFriendRequest(user.id);
+                                            user.status = 1;
+                                        }}>
+                                            ACP
+                                        </button>)}
                                     </div>
                                 );
                             })}
