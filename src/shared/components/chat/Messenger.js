29207d094cc85e38fafd8b5805728432b5993275
ng-anhhtuann <nguyenanhtuan1232@gmail.com>
2023-07-13 13:15:12 +0700

    CSS for no friend

--- src/shared/components/chat/Messenger.js
+++ src/shared/components/chat/Messenger.js
@@ -1,15 +1,22 @@
 import ChatHeader from './ChatHeader';
 import MessageBox from './MessageBox';
 import MessageInput from './MessageInput';
-const Messenger = () => {
+const Messenger = ({currentChat}) => {
     return (
         <div id="messenger" className="card-shadow">
-            <div id="messenger-wrap">
-                <ChatHeader />
-                <hr />
-                <MessageBox />
-                <MessageInput />
-            </div>
+            {currentChat !== undefined 
+            ?  (<div id="messenger-wrap">
+                    <ChatHeader nameDisplay={currentChat.nameDisplay}/>
+                    <hr />
+                    <MessageBox />
+                    <MessageInput />
+                </div>)
+            :   (<div id="centered-none-div">
+                    <h1>Welcome to the chat</h1>
+                    <h1>Please add one more for starting a chat with them</h1>
+                    <h1>Once they accept, refresh the page</h1>
+                    <h1>Enjoy ~</h1>
+                </div>)}
         </div>
     );
 };
